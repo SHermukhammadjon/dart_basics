e8516941af3fe9047bf312606f372f503d914df3
SHermukhammadjon <shermukhammadtemirov@gmail.com>
2023-10-17 12:05:25 +0500

    I finished dynamic list and set lessons in index 12

---
+++ section1/index12.dart
@@ -0,0 +1,18 @@
+// Dynamic lists
+
+
+void main(List<String> args) {
+  List<dynamic> numbers = [];
+
+  numbers.add(3);
+  numbers.add("balh1");
+
+  print(numbers);
+
+
+  Set set = {1, 2, 3, 4, 5};
+  set.add(5);
+  set.add(6);
+  
+  print(set);
+}
