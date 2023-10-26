a1774517b833295f77e7c110ada3260c78768906
SHermukhammadjon <shermukhammadtemirov@gmail.com>
2023-10-26 17:45:02 +0500

    I retarted

--- first_app/lib/main.dart
+++ first_app/lib/main.dart
@@ -1,85 +1,71 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 
+
 void main(List<String> args) {
-  runApp(const MyApp());
+  runApp(MyAppp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class MyAppp extends StatelessWidget
+{
+  MyAppp({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context)
+    {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      theme: ThemeData(primaryColor: Colors.amber),
-      home: MyStateFull(0),
-    );
-  }
-}
+      title: "Defolt Tizim",
+      initialRoute: '/home',
+      routes: {
+        '/home' : (context) => MyHomePage()
+      }
 
-class MyStateFull extends StatefulWidget {
-  int n = 0;
-  MyStateFull(this.n, {super.key});
-
-  void add() {
-    n++;
+      );
+    }
+    
   }
 
-  @override
-  MyStateFullWidget createState() => MyStateFullWidget();
-}
+class MyHomePage extends StatelessWidget
+{
+  const MyHomePage({super.key});
 
-class MyStateFullWidget extends State<MyStateFull> {
   @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-        appBar: AppBar(title: const Text("My app")),
-        // floatingActionButton: FloatingActionButton(
-        //     onPressed: () {
-        //       setState(() {
-        //         widget.n++;
-        //       });
-        //     },
-        // child: const Icon(Icons.add)),
-        body: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
+  Widget build(BuildContext context)
+    {
+      return Scaffold(
+        drawer: Drawer(
+          child: ListView(
+            padding: EdgeInsets.zero,
             children: <Widget>[
-              Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: <Widget>[
-                    const Text("Butooon Pressed time:",
-                        style: TextStyle(fontSize: 25.0, color: Colors.blue)),
-                  ]),
-              Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: <Widget>[
-                    Text("${widget.n}",
-                        style: TextStyle(
-                            fontSize: 30.0,
-                            color: widget.n >= 0 ? Colors.green : Colors.red)),
-                  ]),
-              Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: <Widget>[
-                    Container(
-                        margin: EdgeInsets.all(16.0),
-                        child: ElevatedButton(
-                            onPressed: () {
-                              setState(() {
-                                widget.n--;
-                              });
-                            },
-                            child: const Icon(Icons.remove))),
-                    Container(
-                        margin: EdgeInsets.all(16.0),
-                        child: ElevatedButton(
-                            onPressed: () {
-                              setState(() {
-                                widget.n++;
-                              });
-                            },
-                            child: const Icon(Icons.add))),
-                  ])
-            ]));
-  }
+              const UserAccountsDrawerHeader(
+                accountName: Text('John Doe'),
+                accountEmail: Text('johndoe@example.com'),
+                currentAccountPicture: CircleAvatar(
+                backgroundImage: Ima
+              ),
+            ),
+            ListTile(
+              title: Text('Item 1'),
+              onTap: () {
+                // Handle item 1 tap
+              },
+            ),
+            ListTile(
+              title: Text('Item 2'),
+              onTap: () {
+                // Handle item 2 tap
+              },
+            ),
+          ],
+        ),),
+
+
+
+        appBar: AppBar(title: const Text("Hello")),
+        // body: ListView.builder(
+        //   itemBuilder: ),
+      );
+    }
 }
