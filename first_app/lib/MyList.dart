6eaffd14806b743694c7ed11571792494acb0ea2
SHermukhammadjon <shermukhammadtemirov@gmail.com>
2023-10-27 11:10:00 +0500

    I added navigator page

--- first_app/lib/MyList.dart
+++ first_app/lib/MyList.dart
@@ -1,61 +1,61 @@
 // import 'package:flutter/material.dart';
-import 'dart:io';
+// import 'dart:io';
 
-// class MyList extends StatelessWidget {
-//   const MyList({super.key});
+// // class MyList extends StatelessWidget {
+// //   const MyList({super.key});
 
-//   @override
-//   Widget build(BuildContext context) {
-//     return ListView.builder(
-//       itemCount: 20,
-//       itemBuilder: (context, index) => Container(
-//           child: const Card(
-//               color: Colors.blueAccent,
-//               elevation: 30,
-//               margin: EdgeInsets.symmetric(horizontal: 8, vertical: 10),
-//               child: ListTile(
-//                 leading: CircleAvatar(child: Icon(Icons.abc)),
-//                 title: Text("test"),
-//                 subtitle: Text("Kirish subtile"),
-//                 trailing: Icon(Icons.add_circle_outline_outlined),
-//               ))),
-//     );
-//   }
-// }
+// //   @override
+// //   Widget build(BuildContext context) {
+// //     return ListView.builder(
+// //       itemCount: 20,
+// //       itemBuilder: (context, index) => Container(
+// //           child: const Card(
+// //               color: Colors.blueAccent,
+// //               elevation: 30,
+// //               margin: EdgeInsets.symmetric(horizontal: 8, vertical: 10),
+// //               child: ListTile(
+// //                 leading: CircleAvatar(child: Icon(Icons.abc)),
+// //                 title: Text("test"),
+// //                 subtitle: Text("Kirish subtile"),
+// //                 trailing: Icon(Icons.add_circle_outline_outlined),
+// //               ))),
+// //     );
+// //   }
+// // }
 
-class Page {
-  String title, info, contextFilePath, context = "Null";
-  Icon icon;
+// class Page {
+//   String title, info, contextFilePath, context = "Null";
+//   Icon icon;
 
-  Page(
-      {this.title = "Tile",
-      this.info = "Info",
-      this.contextFilePath = "book/kirish.txt",
-      this.icon = const Icon(Icons.abc)});
+//   Page(
+//       {this.title = "Tile",
+//       this.info = "Info",
+//       this.contextFilePath = "book/kirish.txt",
+//       this.icon = const Icon(Icons.abc)});
 
-  String getContext() {
-    File file = File(contextFilePath);
-    if (file.existsSync()) {
-      return file.readAsStringSync();
-    }
-    return "Null";
-  }
-}
-
-// class Book {
-//   String path;
-//   List<String> files;
-//   Book(this.path, {this.files = const []});
-
-//   List<Widget> getPages() {
-//     String file;
-//     for (file in files) {
-//       File file = File();
+//   String getContext() {
+//     File file = File(contextFilePath);
+//     if (file.existsSync()) {
+//       return file.readAsStringSync();
 //     }
+//     return "Null";
 //   }
 // }
 
-void main()
-{
-  print("blah");
-}
+// // class Book {
+// //   String path;
+// //   List<String> files;
+// //   Book(this.path, {this.files = const []});
+
+// //   List<Widget> getPages() {
+// //     String file;
+// //     for (file in files) {
+// //       File file = File();
+// //     }
+// //   }
+// // }
+
+// void main()
+// {
+//   print("blah");
+// }
--- first_app/lib/main.dart
+++ first_app/lib/main.dart
@@ -1,71 +1,88 @@
 import 'dart:io';
-
 import 'package:flutter/material.dart';
-
 
 void main(List<String> args) {
   runApp(MyAppp());
 }
 
-class MyAppp extends StatelessWidget
-{
+class MyAppp extends StatelessWidget {
   MyAppp({super.key});
 
   @override
-  Widget build(BuildContext context)
-    {
+  Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: "Defolt Tizim",
-      initialRoute: '/home',
-      routes: {
-        '/home' : (context) => MyHomePage()
-      }
-
-      );
-    }
-    
+        debugShowCheckedModeBanner: false,
+        title: "Defolt Tizim",
+        initialRoute: '/page',
+        routes: {
+          '/home': (context) => const MyHomePage(),
+          '/page': (context) => const Page()
+        });
   }
+}
 
-class MyHomePage extends StatelessWidget
-{
+class MyHomePage extends StatelessWidget {
   const MyHomePage({super.key});
 
   @override
-  Widget build(BuildContext context)
-    {
-      return Scaffold(
+  Widget build(BuildContext context) {
+    return Scaffold(
         drawer: Drawer(
-          child: ListView(
-            padding: EdgeInsets.zero,
-            children: <Widget>[
-              const UserAccountsDrawerHeader(
-                accountName: Text('John Doe'),
-                accountEmail: Text('johndoe@example.com'),
+          child: ListView(padding: EdgeInsets.zero, children: <Widget>[
+            const UserAccountsDrawerHeader(
+                accountName: Text('Temirov SHermukhammad'),
+                accountEmail: Text('temirovshermukhammad@gmail.com'),
                 currentAccountPicture: CircleAvatar(
-                backgroundImage: Ima
-              ),
-            ),
-            ListTile(
-              title: Text('Item 1'),
-              onTap: () {
-                // Handle item 1 tap
-              },
-            ),
-            ListTile(
-              title: Text('Item 2'),
-              onTap: () {
-                // Handle item 2 tap
-              },
-            ),
-          ],
-        ),),
-
-
-
+                  backgroundImage:
+                      NetworkImage("https://picsum.photos/200/200?random=1000"),
+                )),
+            const Text("Blah")
+          ]),
+        ),
         appBar: AppBar(title: const Text("Hello")),
-        // body: ListView.builder(
-        //   itemBuilder: ),
-      );
-    }
+        body: ListView.builder(
+            itemCount: 15,
+            itemBuilder: (BuildContext context, int index) {
+              return Container(
+                  margin: EdgeInsets.all(10),
+                  width: 50,
+                  height: 100,
+                  color: Colors.blueAccent,
+                  child: ListTile(
+                    leading: Image.network(
+                      "https://picsum.photos/200/200?random=${100 + index}",
+                    ),
+
+                    //  Image.network(
+                    //     "https://picsum.photos/200/200?random=${100 + index}"),
+                    title: Text("Blah $index"),
+                    subtitle: const Text("Subtitle"),
+                    onTap: () => Navigator.pushNamed(context, '/page'),
+                  ));
+            }));
+  }
+}
+
+class Page extends StatelessWidget {
+  const Page({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+            title: const Text("Ok ok"),
+            leading: IconButton(
+              icon: const Icon(Icons.arrow_back),
+              onPressed: () {
+                Navigator.pushNamed(context, '/home');
+              },
+            )),
+        body: ListView(children: const  <Widget>
+        [
+          Text("Blahe"),
+          Text("Blahe"),
+          Text("Blahe")
+        ])
+        );
+  }
 }
